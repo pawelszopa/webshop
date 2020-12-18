a5643b653e3fd24a246e8a73684a6e2e83ff730a
Pawel Szopa <expiren@gmail.com>
2020-12-18 18:53:52 +0100

    item endpoint

--- alegrosz/views/item_view.py
+++ alegrosz/views/item_view.py
@@ -40,3 +40,36 @@ def add():
         return redirect(url_for("main.index"))
 
     return render_template('add.html', form=form)
+
+
+@bp_item.route('/<int:item_id>', methods=["GET"])
+def item(item_id):
+
+    c = get_db().cursor()
+    c.execute('''SELECT
+            i.id, i.title, i.description, i.price, i.image, c.name, s.name
+            FROM 
+            item AS i
+            INNER JOIN category AS c ON i.category_id = c.id
+            INNER JOIN subcategory AS s ON i.subcategory_id = s.id
+            WHERE i.id = ?
+        ''', (item_id,))
+
+    row = c.fetchone()
+
+    try:
+        db_item = {
+            'id': row[0],
+            'title': row[1],
+            'description': row[2],
+            'price': row[3],
+            'image': row[4],
+            'category_name': row[5],
+            'subcategory_name': row[6],
+        }
+    except IndexError:
+        db_item = {}
+
+    if db_item:
+        return render_template('item.html', item=db_item)
+
